0a242482416839a78a8d74c5178f2afe4bac1293
Jan Mizerka <50331798+Johny198@users.noreply.github.com>
2022-03-04 23:46:15 +0100

    Update main.py

--- ssvep/main.py
+++ ssvep/main.py
@@ -1,20 +1,21 @@
 from multiprocessing import Process, Value
 
-def get_bci_data(state):
+def get_bci_data(state, is_finished):
     from ssvep.modules.signal import display
-    display.run(state)
+    display.run(state, is_finished)
 
-def display_stimuli(state):
+def display_stimuli(state, is_finished):
     from ssvep.modules.stimuli import stimulus
-    stimulus.draw(state)
+    stimulus.draw(state, is_finished)
 
 
 
 
 if __name__ == '__main__':
     state = Value('i',0)
-    proc1 = Process(target = get_bci_data, args = (state,))
-    proc2 = Process(target = display_stimuli, args = (state,))
+    is_finished = Value('i',0)
+    proc1 = Process(target = get_bci_data, args = (state,is_finished))
+    proc2 = Process(target = display_stimuli, args = (state,is_finished))
     proc2.start()
     proc1.start()
     proc2.join()

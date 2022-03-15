91c657493efc4550a9279e0867e8b65d39ea82b6
Jan Mizerka <50331798+Johny198@users.noreply.github.com>
2022-03-15 17:58:06 +0100

    Update main.py

--- ssvep/main.py
+++ ssvep/main.py
@@ -1,22 +1,30 @@
-from multiprocessing import Process, Value
+from multiprocessing import Process, Value, Array
 
-def get_bci_data(state, is_finished):
+
+def get_bci_data(state,is_finished,mp_array):
     from ssvep.modules.signal import display
-    display.run(state, is_finished)
+    display.run(state, is_finished,mp_array) 
 
-def display_stimuli(state, is_finished):
+def display_stimuli(state,is_finished):
     from ssvep.modules.stimuli import stimulus
-    stimulus.draw(state, is_finished)
+    stimulus.draw(state,is_finished)
 
-
-
+def extract(mp_array,is_finished):
+    from ssvep.modules.signal import extraction
+    extraction.cca(mp_array,is_finished)
 
 if __name__ == '__main__':
     state = Value('i',0)
+    last_state = Value('i',0)
     is_finished = Value('i',0)
-    proc1 = Process(target = get_bci_data, args = (state,is_finished))
-    proc2 = Process(target = display_stimuli, args = (state,is_finished))
+    mp_array = Array('d', 2404)
+
+    proc1 = Process(target = get_bci_data, args = (state,is_finished,mp_array))
+    proc2 = Process(target = display_stimuli, args = (state, is_finished))
+    proc3 = Process(target = extract, args = (mp_array, is_finished))
     proc2.start()
     proc1.start()
+    proc3.start()
     proc2.join()
     proc1.join()
+    proc3.join()
